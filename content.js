8c5f4e6442886da68cdf71bb8639a26ec13f3e04
lveeeeeeeeeee <94752867+lveeeeeeeeeee@users.noreply.github.com>
2023-08-08 12:56:16 +0500

    Added shorts container removal using XPath

--- content.js
+++ content.js
@@ -5,4 +5,25 @@ const interval = setInterval(() => {
   nodes.forEach((n) => {
     n.style.filter = "blur(20px)";
   });
+
+  try {
+    // searching shorts container with xpath
+    let short = document.evaluate(
+    "//h3/*[contains(@href, '/shorts')]/ancestor::ytd-rich-section-renderer[contains(@use-show-more-button, '')]",
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+
+  for (let i = 0; i < short.snapshotLength; i++) {
+    console.log("deleted shorts suggestions renderer " + short.snapshotItem(i).nodeName);
+    short.snapshotItem(i).remove();
+  }
+  }
+  
+  catch(e)
+  {
+    console.log(e);
+  }
 }, 1);
